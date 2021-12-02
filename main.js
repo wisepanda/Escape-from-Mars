d75e8888c1b4f45f524351419767c0935e0a982d
ivanf <ivan.fanello@gmail.com>
2021-12-02 19:14:52 +0000

    task one and two completed

---
+++ main.js
@@ -0,0 +1,76 @@
+// GETGet logs
+// https://task-escape-api.herokuapp.com/api/logs
+
+// const { json } = require("stream/consumers");
+
+// Retrieve all of the logs of our mission so far
+
+
+async function getLogs(){
+    Response = await fetch("https://task-escape-api.herokuapp.com/api/logs"); //{method: "GET",redirect:"follow"});
+    let data = await Response.json();
+    console.log(data.date)
+}
+getLogs()
+
+
+
+
+
+// Example Request
+// Get logs
+// curl --location --request GET 'https://task-escape-api.herokuapp.com/api/logs'
+// GETGet personnel
+// https://task-escape-api.herokuapp.com/api/personnel/1
+// Retrieves the relevant personnel record matching the id provided in the path
+
+
+
+
+
+
+
+// Example Request
+// Get personnel
+// curl --location --request GET 'https://task-escape-api.herokuapp.com/api/personnel/1'
+
+async function getPerson(){
+    let Response = await fetch("https://task-escape-api.herokuapp.com/api/personnel/9");
+        data = await Response.json();
+        console.log(data)
+        return data
+}
+getPerson()
+
+
+
+
+// GETGet messages
+// https://task-escape-api.herokuapp.com/api/messages?to=1
+// Retrieves a list of messages filtered by recipient, where the recipient id matches the to value
+
+// PARAMS
+// to
+// 1
+
+
+// Example Request
+// Get messages
+// curl --location --request GET 'https://task-escape-api.herokuapp.com/api/messages?to=1'
+// GETGet hint
+// https://task-escape-api.herokuapp.com/api/hint
+// Returns some information to help understand what is required from a launch code
+
+
+
+// Example Request
+// Get hint
+// curl --location --request GET 'https://task-escape-api.herokuapp.com/api/hint'
+// POSTSubmit launch codes
+// https://task-escape-api.herokuapp.com/api/codes
+// Allows the submission of launch codes to the system via a JSON object as the body of the request
+
+// BODYraw
+// {
+//     "enter": "LAUNCH"
+// }
